cd115b6d4f11b84c1ca2307eec67077c0afc57ef
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-05-03 11:26:04 -0400

    15 done

--- problems/15-subsets.js
+++ problems/15-subsets.js
@@ -14,7 +14,19 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
      subset that is the same, except it also does contain 3.
 ***********************************************************************/
 
-// your code here
+const subsets = (arr, res = []) => {
+  const num = arr.length;
+  build(arr, res, 0, num, []);
+  return res;
+}
+
+const build = (arr, res, start, num, cur) => {
+  res.push(cur);
+  if (start === num) return;
+  for (let i = start; i < num; i++) {
+    build(arr, res, i + 1, num, [...cur, arr[i]]);
+  }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
